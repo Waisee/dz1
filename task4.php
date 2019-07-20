e8c24b3a4f9b519740fd23f9ba5c64f7c9e565c7
Danil <waisee7@gmail.com>
2019-07-20 16:15:14 +0300

    - fourth task

---
+++ task4.php
@@ -0,0 +1,14 @@
+<?php
+
+$day = rand(0, 10);
+
+switch ($day) {
+    case ($day >= 1 && $day <= 5):
+        echo 'Это рабочий день';
+        break;
+    case ($day == 6 || $day == 7):
+        echo 'Это выходной день';
+        break;
+    default:
+        echo 'Неизвестный день';
+}
